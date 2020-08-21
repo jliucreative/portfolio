e1077e314ae16bbc85b44592fbd4ee7ff571e1d5
Joanne Liu <joli3145@colorado.edu>
2020-08-21 15:15:51 -0600

    Updates

---
+++ precache-manifest.2493fcaf1ab9c514cff8de5d93263f54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a661279035f7ba0efb89fa9f731fa4e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f30b2c7343a4d4af3bac",
+    "url": "/static/css/main.c9e7a6a0.chunk.css"
+  },
+  {
+    "revision": "7331aa7d5f265e90d7e3",
+    "url": "/static/js/2.efd0c370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.efd0c370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30b2c7343a4d4af3bac",
+    "url": "/static/js/main.0f5d385f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "92c47177de4c25982e81c96b4050287a",
+    "url": "/static/media/email.92c47177.png"
+  },
+  {
+    "revision": "26c7771a5ae57d0187b832d6be34018d",
+    "url": "/static/media/linkedIn.26c7771a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.44bd17f372dd0893963a4e3aded2b3c1.js"
+  "/precache-manifest.2493fcaf1ab9c514cff8de5d93263f54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
